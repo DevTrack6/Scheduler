083e8435abb9401f5cc1485735967d2d1d19b086
terry <munu220@gmail.com>
2023-08-28 07:27:47 +0000

    [feat] schedule model 추가

--- scheduler/schedule_app/admin.py
+++ scheduler/schedule_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Group, Todo, User
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Group)
+admin.site.register(Todo)
